567663aa9a6591f093ef316c80e97158216fc5fe
1894220 <upadhyayMoni2>
2019-06-19 10:43:37 -0400

    arraylist itreable

---
+++ app/src/main/java/com/example/a1894220/firebasedemo/Github.java
@@ -0,0 +1,31 @@
+package com.example.a1894220.firebasedemo;
+
+public class Github {
+
+    int id,watchers;
+    String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getWatchers() {
+        return watchers;
+    }
+
+    public void setWatchers(int watchers) {
+        this.watchers = watchers;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- app/src/main/java/com/example/a1894220/firebasedemo/MainActivity.java
+++ app/src/main/java/com/example/a1894220/firebasedemo/MainActivity.java
@@ -13,15 +13,19 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView Txt1,Txt2;
+    ArrayList<Github> arrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        arrayList=new ArrayList<>();
 
         FirebaseApp.initializeApp(this);
 
@@ -36,13 +40,33 @@ public class MainActivity extends AppCompatActivity {
                 // whenever data at this location is updated.
                // String value = dataSnapshot.getValue(String.class);
 
-                DataSnapshot snap=dataSnapshot.child("0");
+
+
+                /*DataSnapshot snap=dataSnapshot.child("0");
                 int watch= Integer.parseInt(snap.child("watchers").getValue().toString());
                 String name = snap.child("full_name").getValue().toString();
+                String name1=snap.child("full_name").getValue(String.class);
 
 
-                 Txt1.setText(name);
-               Txt2.setText(watch);
+                Txt1.setText(name);
+               Txt2.setText(watch); */
+
+                Iterable<DataSnapshot> childs=dataSnapshot.getChildren();
+
+                    for(DataSnapshot snaps : childs){
+
+                        Github gt=snaps.getValue(Github.class);
+                        System.out.println(gt.getId()+" "+gt.getName()+""+gt.getWatchers());
+                        arrayList.add(gt);
+
+                    }
+
+
+
+                      System.out.print("size of array"+arrayList.size());
+                    for(int i=0;i<arrayList.size();i++){
+                        System.out.print(arrayList.get(i).getId()+""+arrayList.get(i).getName()+""+arrayList.get(i).getWatchers());
+                    }
 
             }
 
